ab3b7490903e1a2b6c7631362160b5c907f004a5
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-02-16 11:53:14 -0300

    Both servers are tested and online Front and BackEnd

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3200;
+
+app.get('/start', (req, res) => {
+    res.send('ok')
+})
+
+app.listen(PORT, () => {
+  console.log(`project running on PORT ${PORT}`)  
+})
